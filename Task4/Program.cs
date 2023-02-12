5fa61ea4a71158776e552d37b5af8cf22b9b3112
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-12 21:46:38 +0300

    add Задача 4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, принимающая на вход число (N), а на выходе
+// показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine(), out int number); // вводим число
+
+    var even = 1; 
+
+    while (even <= number)
+    {
+      if (even % 2 == 0)
+      {
+        Console.Write(even + " ");
+      }
+        even++;
+    }
