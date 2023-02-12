0f7f7946d8553040d72aaf81ad6da2348fd07f4a
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-12 20:59:19 +0300

    add Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход число и выдает, является ли число четным.
+
+Console.WriteLine("Проверяем число на четность!");
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine(), out int N);
+
+var Prov = N % 2;
+
+  if (Prov == 0)
+  {
+    Console.WriteLine("Вы ввели чётное число!");
+  }
+  else
+  {
+    Console.WriteLine("Вы ввели нечётное число!");
+  }  
