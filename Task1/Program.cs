dce849c7ee067124afe840c0e5b085775249cf03
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-12 14:42:31 +0300

    add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа, которая принимает на вход 2 числа и выдает,
+// какое число большее, какое меньшее.
+
+//int numberA = -8;
+//int numberB = -10;
+
+Console.Write("Введите первое число: ");
+int.TryParse(Console.ReadLine(), out int numberA);
+Console.Write("Введите второе число: ");
+int.TryParse(Console.ReadLine(), out int numberB);
+
+if (numberA < numberB)
+{
+  Console.Write("min = ");
+  Console.WriteLine(numberA);
+  Console.Write("max = ");
+  Console.WriteLine(numberB);
+}
+else
+{
+  Console.Write("max = ");
+  Console.WriteLine(numberA);
+  Console.Write("min = ");
+  Console.WriteLine(numberB);
+}
